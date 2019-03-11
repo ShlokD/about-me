f25a4bd74ac52ffe28eadcab1e47408e4b517203
ShlokD <shlokpdesai@gmail.com>
2019-03-10 20:19:57 -0700

    Add project and upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const cssnano = require('gulp-cssnano');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
-gulp.task('build:css', function() {
+gulp.task('default', () => {
   return gulp.src('scss/*.scss')
   .pipe(sass())
   .pipe(cssnext())
@@ -15,21 +15,3 @@ gulp.task('build:css', function() {
   .on('error', sass.logError)
   .pipe(gulp.dest('dist/css'));
 });
-
-gulp.task('serve', function() {
-  browserSync({
-    server: {
-      baseDir: '.'
-    }
-  });
-
-  gulp.watch(['*.html', 'dist/css/**/*.css'], {cwd: '.'}, reload);
-});
-
-gulp.task('build:watch', function() {
-  gulp.watch('scss/**/*.scss', ['build:css']);
-});
-
-
-gulp.task('hot',['serve', 'build:watch']);
-gulp.task('default', ['build:css']);
